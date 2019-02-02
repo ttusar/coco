2b3736611d053a607c0cb62bafc83fda9d01bacf
TheHedgeify <theHedgeify@gmail.com>
2019-02-02 04:27:07 +0000

    forgot to add which suite

--- code-experiments/build/python/rw_example_experiment.py
+++ code-experiments/build/python/rw_example_experiment.py
@@ -113,7 +113,8 @@ def run_experiment(suite_name,
 
 # export LD_LIBRARY_PATH=path_to_rw_top_trumps_library
 if __name__ == '__main__':
-    run_experiment('function_indices: ' + str(sys.argv[1]) + ' instance_indices: ' + str(sys.argv[2]) + ' dimensions: 10',
+    run_experiment('rw-gan-mario',
+		   'function_indices: ' + str(sys.argv[1]) + ' instance_indices: ' + str(sys.argv[2]) + ' dimensions: 10',
                    add_observer_name='rw',
                    add_observer_options='log_only_better: 0 log_variables: all precision_x: 4',
                    alg='lhs', budget_multiplier=50)
