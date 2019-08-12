9e7009f8c286e255a9ba5ca5fe7b6c18ec7d12a1
TheHedgeify <theHedgeify@gmail.com>
2019-08-12 01:51:37 +0100

    fixed indent

--- code-experiments/rw-problems/socket_server.py
+++ code-experiments/rw-problems/socket_server.py
@@ -35,9 +35,9 @@ def evaluate_message(message):
         # Find the right evaluator
         if 'toy-socket' in suite_name:
             evaluate = evaluate_toy_socket
-         elif 'mario-gan' in suite_name:
+        elif 'mario-gan' in suite_name:
              evaluate = evaluate_mario_gan
-         elif 'mario-gan-biobj' in suite_name:
+        elif 'mario-gan-biobj' in suite_name:
              evaluate = evaluate_mario_gan
         # ADD HERE the function for another evaluator, for example
         # elif 'my-suite' in suite_name:
