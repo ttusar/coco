e7dea7da0a7f1bbe92f07fb557577a8f00018c40
Tea Tusar <tea.tusar@ijs.si>
2020-05-15 23:43:40 +0200

    Cleaned up example experiment

--- code-experiments/examples/rw-jmetalpy/rw_experiment_nsga_ii.py
+++ code-experiments/examples/rw-jmetalpy/rw_experiment_nsga_ii.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 """
-An experiment using an algorithm implemented in JMetal (NSGA-II) to solve real-world COCO problems
-that use sockets for evaluating solutions.
+An experiment using some algorithms implemented in JMetal to solve real-world COCO problems that use
+sockets for evaluating solutions.
 
 Arguments:
-    suite=SUITE_NAME                   # Suite name (default top-trumps-biobj)
+    algorithm=ALGORITHM_NAME           # Algorithm name (NSGA-II, GDE3 and RS supported so far)
+    suite=SUITE_NAME                   # Suite name (default rw-top-trumps-biobj)
     suite_options=SUITE_OPTIONS        # Suite options (default '')
     observer=OBSERVER_NAME             # Name of the observer (default 'bbob-biobj')
     observer_options=OBSERVER_OPTIONS  # Observer options (default '')
@@ -22,14 +23,23 @@ The socket server needs to be running when this script is called. This is done v
 `python do.py run-rw-top-trumps-server port=7001 batch=1`
 After the experiment has ended, the socket servers can be stopped via
 `python do.py stop-socket-servers port=7001
+Different batches require different sockets
 """
 import cocoex
 import sys
 from coco_problem import CocoProblem
-from jmetal.algorithm.multiobjective import NSGAII
+from jmetal.algorithm.multiobjective import NSGAII, GDE3, RandomSearch
 from jmetal.util.termination_criterion import StoppingByEvaluations
 from jmetal.operator import SBXCrossover, PolynomialMutation
 from jmetal.util.comparator import DominanceComparator
+import random
+
+
+class RS(RandomSearch):
+    # This had to be redefined to make it work
+    def step(self) -> None:
+        new_solution = self.problem.create_solution()
+        self.problem.evaluate(new_solution)
 
 
 def _get_socket_port(suite_name, start_port, current_batch):
@@ -47,16 +57,20 @@ def _get_socket_port(suite_name, start_port, current_batch):
     
 def parse_arguments(argv):
     """Parses the command-line arguments and returns a dictionary with all the arguments"""
-    # These defaults should match those from do.py (and the documentation above)
-    suite_name = 'rw-top-trumps-biobj'
+    algorithm_name = 'RS'
+    batches = 1
     current_batch = 1
-    start_port = 7000
-    # Other defaults
-    suite_options = ''
+    start_port = 7000  # Do not change
     observer_name = 'bbob-biobj'
     observer_options = ''
-    budget_multiplier = 10
-    batches = 1
+    if 11 < 3:
+        suite_name = 'bbob-biobj-mixint'
+        suite_options = 'dimensions: 5,10 function_indices: 1-92 instance_indices: 1'
+        budget_multiplier = 10000
+    else:
+        suite_name = 'rw-top-trumps-biobj'
+        suite_options = 'dimensions: 88,128 function_indices: 1 instance_indices: 1-5'
+        budget_multiplier = 1000
     # Parse the command line arguments
     for arg in argv:
         if arg[:6] == 'suite=':
@@ -75,10 +89,13 @@ def parse_arguments(argv):
             current_batch = int(arg[6:])
         elif arg[:11] == 'start_port=':
             start_port = int(arg[11:])
+        elif arg[:10] == 'algorithm=':
+            algorithm_name = arg[10:]
     # Get the right port for this suite
     port = _get_socket_port(suite_name, start_port, current_batch)
     # Print out the options
     print('RW experiment ran with the following options:')
+    print('algorithm = {}'.format(algorithm_name))
     print('suite = {}'.format(suite_name))
     print('suite_options = {}'.format(suite_options))
     print('observer = {}'.format(observer_name))
@@ -86,7 +103,7 @@ def parse_arguments(argv):
     print('budget_multiplier = {}'.format(budget_multiplier))
     print('batches = {}'.format(batches))
     print('current_batch = {}'.format(current_batch))
-    return dict(suite_name=suite_name, suite_options=suite_options,
+    return dict(algorithm_name=algorithm_name, suite_name=suite_name, suite_options=suite_options,
                 observer_name=observer_name, observer_options=observer_options,
                 budget_multiplier=budget_multiplier, batches=batches,
                 current_batch=current_batch, port=port)
@@ -95,6 +112,7 @@ def parse_arguments(argv):
 def run_experiment(argv=[]):
     # Parse the command-line arguments
     params = parse_arguments(argv)
+    algorithm_name = params['algorithm_name']
     suite_name = params['suite_name']
     suite_options = params['suite_options']
     observer_name = params['observer_name']
@@ -108,40 +126,53 @@ def run_experiment(argv=[]):
     suite_options = 'port: {} {}'.format(port, suite_options)
     suite = cocoex.Suite(suite_name, '', suite_options)
     # Prepare the observer
-    observer = cocoex.Observer(observer_name, '{} result_folder: {}-{}{}'.format(
-        observer_options, suite_name, 'NSGA-II',
-        '-batch-{}'.format(current_batch) if batches > 1 else ''))
+    observer = cocoex.Observer(observer_name, '{} result_folder: {}-{}{} algorithm_name:{}'.format(
+        observer_options, suite_name, algorithm_name,
+        '-batch-{}'.format(current_batch) if batches > 1 else '', algorithm_name))
     # Use minimal printing
     minimal_print = cocoex.utilities.MiniPrint()
 
-    # Run the solver on the problems of the suite in the current batch
+    # Run the algorithm on the problems of the suite in the current batch
     for problem_index, problem in enumerate(suite):
         # Skip problems not in this batch
         if (problem_index + current_batch - 1) % batches:
             continue
         # Observe the problem
         problem.observe_with(observer)
-        # Population size must be an even number
-        population_size = max(int(problem.dimension / 2), 10)
-        population_size += 1 if population_size % 2 != 0 else 0
-        # Restart the solver while neither the problem is solved nor the budget is exhausted
-        # (not particularly useful here since NSGAII does not (yet) support restarts)
-        while (problem.evaluations < problem.dimension * budget_multiplier
-               and not problem.final_target_hit):
-            observer.signal_restart(problem)
-            # Use jMetalPy's NSGA-II algorithm
+        # Set random seed
+        random.seed(69)
+        # Use the chosen jMetalPy's algorithm
+        if (algorithm_name == 'NSGA-II') or (algorithm_name == 'NSGAII'):
             algorithm = NSGAII(
                 problem=CocoProblem(problem),
-                population_size=population_size,
-                offspring_population_size=population_size,
+                population_size=20,
+                offspring_population_size=20,
                 mutation=PolynomialMutation(
                     probability=1.0 / problem.dimension, distribution_index=20),
                 crossover=SBXCrossover(probability=1.0, distribution_index=20),
                 termination_criterion=StoppingByEvaluations(
-                    max_evaluations=budget_multiplier * problem.dimension - problem.evaluations),
+                    max_evaluations=budget_multiplier * problem.dimension),
                 dominance_comparator=DominanceComparator()
             )
-            algorithm.run()
+        elif algorithm_name == 'GDE3':
+            algorithm = GDE3(
+                problem=CocoProblem(problem),
+                population_size=20,
+                cr=0.5,
+                f=0.5,
+                termination_criterion=StoppingByEvaluations(
+                    max_evaluations=budget_multiplier * problem.dimension),
+                dominance_comparator=DominanceComparator()
+            )
+        elif algorithm_name == 'RS':
+            algorithm = RS(
+                problem=CocoProblem(problem),
+                termination_criterion=StoppingByEvaluations(
+                    max_evaluations=budget_multiplier * problem.dimension)
+            )
+        else:
+            raise ValueError('Algorithm {} not supported (yet)'.format(algorithm_name))
+        algorithm.run()
         minimal_print(problem, final=problem.index == len(suite) - 1)
 
 
